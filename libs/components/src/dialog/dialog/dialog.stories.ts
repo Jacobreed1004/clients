6446c13f16c2ed32302585484b44bbc7085a2e33
Andreas Coroiu <andreas.coroiu@gmail.com>
2022-08-31 14:23:56 +0200

    [EC-457] fix: align trash icon right

--- libs/components/src/dialog/dialog/dialog.stories.ts
+++ libs/components/src/dialog/dialog/dialog.stories.ts
@@ -50,7 +50,7 @@ const Template: Story<DialogComponent> = (args: DialogComponent) => ({
     <div bit-dialog-footer class="tw-flex tw-items-center tw-flex-row tw-gap-2">
       <button bitButton buttonType="primary">Save</button>
       <button bitButton buttonType="secondary">Cancel</button>
-      <button bitIconButton="bwi-trash" buttonType="danger" size="default"></button>
+      <button class="tw-ml-auto" bitIconButton="bwi-trash" buttonType="danger" size="default"></button>
     </div>
   </bit-dialog>
   `,
